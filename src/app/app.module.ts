aee92d1d9836e91b489085bc79926aee9d6a7380
Philip Clement <pclement803@gmail.com>
2023-04-08 11:12:25 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Add this import
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortableTableComponent } from './SortableTableComponent/sortable-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortableTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
